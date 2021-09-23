76cacf778eda4a73f5dce4c2561fde9bce178946
shreyas karanam <shreyaskaranam22@gmail.com>
2021-09-23 23:50:42 +0530

    restaurants data requested and responsed as json on the server

---
+++ backend/api/restaurants.controller.js
@@ -0,0 +1,30 @@
+import RestaurantsDAO from "../dao/restaurantsDAO.js"
+
+export default class restaurantscontroller{
+  static async apiGetRestaurants(req,res,next){
+    const restaurantsPerPage = req.query.resturantsPerPage ? parseInt(req.query.restaurantsPerPage,10):20
+    const page = req.query.page ? parseInt(req.query.page,10):0
+
+    let filters = {}
+    if(req.query.cusine){
+      filter.cusine = req.query.cusine
+    }else if(req.query.zipcode){
+      filter.zipcode = req.query.zipcode
+    }else if(req.query.name){
+      filter.name = req.query.name
+    }
+    const {restaurantsList, totalNumRestaurants} = await RestaurantsDAO.getRestaurants ({
+      page,
+      filters,
+      restaurantsPerPage,
+    })
+    let response = {
+      filters : filters,
+      restaurants:restaurantsList,
+      page : page,
+      entries_per_page: restaurantsPerPage,
+      total_results: totalNumRestaurants,
+    }
+    res.json(response)
+  }
+}
--- backend/dao/restaurantsDAO.js
+++ backend/dao/restaurantsDAO.js
@@ -1,6 +1,6 @@
 let restaurants //variable
-
-export default restaurantsDAO{ //exporting this specfic page
+//DA) => Data Access Object
+export default class RestaurantsDAO { //exporting this specfic page
   static async injectDB(conn){ // async-await is promise returning keywords
     if(restaurants){
       return
@@ -13,21 +13,43 @@ export default restaurantsDAO{ //exporting this specfic page
       console.error(`Unable to connect to the database ${e}`,)
     }
   }
+  static async getRestaurants({
+    filters = null,
+    page = 0,
+    restaurantsPerPage = 20,
+  } = {})
+  {
+    let query
+    if(filters){
+    if("name" in filters){
+      query = {$text:{$search: filters["name"] } } //search w.r.t name given by the user
+    }
+    else if("cusine" in filters){
+      query = {"cusine":{$eq: filters["cusine"] } } // search w.r.t cusine of the restaurants
+    }
+    else if("zipcode" in filters){
+      query = {"address.zipcode": {$eq: filters["zipcode"] } } // search w.r.t zipcode of the restaurants
+    }
+  }
+  let cursor
+  try{
+    cursor = await restaurants.find(query) //trying to get queries from top and store in cursor
+  }
+  catch(e){
+    console.error(`Unable to fetch query results ${e}`)
+    return{restaurantsList: [], totalNumRestaurants: 0} //if no results are found we initialize the 2 to nothing
+  }
+  const displayCursor = cursor.limit(restaurantsPerPage).skip(restaurantsPerPage*page)
+
+  try{
+    const restaurantsList = await displayCursor.toArray()
+    const totalNumRestaurants = await restaurants.countDocuments(query)
+
+    return {restaurantsList, totalNumRestaurants}
+  }
+  catch(e){
+    console.error(`Unable to fetch results ${e}`)
+    return {restaurantsList: [],totalNumRestaurants: 0}
+  }
 }
-static async getRestaurants({
-  filters = null,
-  page = 0,
-  restaurantsPerPage = 20,
-} = {})
-{
-  let query
-  if("name" in filters){
-      query = {$text:{$search: filters["name"] } }
-  }
-  else if("cusine" in filters){
-    query = {"cusine":{$eq: filters["cusine"] } }
-  }
-  else if("zipcode" in filters){
-    query = {"address.zipcode": {$eq: filters["zipcode"] } }
-  }
 }
