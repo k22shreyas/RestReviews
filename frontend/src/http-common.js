77d39a9e99d89f0a28579fc5f461520a35a1e484
k22shreyas <shreyaskaranam22@gmail.com>
2022-03-29 23:37:31 +0530

    realm working except search

---
+++ frontend/src/http-common.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://ap-south-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/restreviews-ilvex/service/restaurants/incoming_webhook/",
+  headers: {
+    "Content-type": "application/json"
+  }
+});
